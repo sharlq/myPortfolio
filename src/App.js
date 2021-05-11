5652f09ba2fc2833ced5242af86ba35431dec6f4
sharlq <shehabakhader@gmail.com>
2021-05-11 10:21:14 +0300

    finally i figuredout what i want to do

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Intro from './components/intro'
 import My from "./components/Myprojects"
+import Nav from "./components/NavBar"
 function App() {
   return (
     <div className="app">
+    <Nav />
     <Intro />
-    <My />
     </div>
   );
 }
