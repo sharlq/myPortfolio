aa3ef3b79a353b54fba17c66092a940e48ceffab
sharlq <shehabakhader@gmail.com>
2021-05-13 14:05:20 +0300

    ended the projects section

---
+++ src/components/MySkills/MySkills.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const MySkills = () => {
+    return (
+        <div className="mySkills">
+            <h1 className="mySkills-title">My Skills</h1>
+            <div className="mySkills-grid">
+            <div className="programmingSkills">
+                <h1 className="programmingSkills-title">Programming Skills</h1>
+            </div>
+            <div className="otherSkills">
+
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default MySkills
---
+++ src/components/Myprojects/cardsData.js
@@ -0,0 +1,31 @@
+const  cards =[
+    {
+      title: 'Sorting Algorithms Visualizer',
+      description:
+        'It has four sorting algorithems  Quick sort, Merge sort, Insertion sort and Bubble sort also it gives you the ability to control the speed of the visualization and the size of the data set ',
+      image: 'https://www.mediafire.com/convkey/b076/g0b34zrudjjezyyzg.jpg',
+      imageRatio:  1080/1920,
+      preview:'https://sorting-algorithms-visualizerr.netlify.app/',
+      github:'https://github.com/sharlq/sortingAlgorithmsVisualizedr'
+    },
+    {
+      title: 'Real time chat',
+      description:
+        'Used nodeJs to create the backend and socket.io for real time  bidirectional data transfer',
+      image: 'https://www.mediafire.com/convkey/e146/gylbhztevir54xjzg.jpg',
+      imageRatio:  1080/1920,
+      preview:'https://real-time-chat-app-shehab.netlify.app/',
+      github:'https://github.com/sharlq/chat-app-react-nodejs-socket.io'
+    },
+    {
+      title: 'Ecommerce',
+      description:
+        'in this project used CommerceJS for backend and stripe for payment and money transaction ',
+      image: 'https://www.mediafire.com/convkey/6047/u3q5jcvczrg79mczg.jpg',
+      imageRatio:  1080/1920,
+      preview:'https://portfolio-ecommercejs.netlify.app/',
+      github:'https://github.com/sharlq/React_Ecommerce'
+    }]
+
+
+    export default cards
