a79b6325ac292c03ad8cd7048f0da74c27d567a3
sharlq <shehabakhader@gmail.com>
2021-05-15 00:55:02 +0300

    created the about me section

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import Intro from './components/intro/intro'
 import MyProjects from "./components/Myprojects/Myprojects"
 import Nav from "./components/NavBar"
 import MySkills from './components/MySkills/MySkills'
+import AboutMe from './components/AboutMe/AboutMe'
 function App() {
   return (
+    <>
     <div className="app">
     <Nav />
     <Intro />
     <MyProjects />
     <MySkills />
     </div>
+    <AboutMe />
+    </>
   );
 }
 
--- src/components/MySkills/MySkills.jsx
+++ src/components/MySkills/MySkills.jsx
@@ -15,12 +15,14 @@ const MySkills = () => {
                     <ul className="skills-list">
                         <li>Strong Knowledge in the HTML5, CSS3, JavaScrpt and EcmaScript</li>
                         <li>Experience with REACT</li>
+                        <li>BootStrap and Material UI</li>
                         <li>Knowledge of webStructure and browser performance</li>
                         <li>Familiarity with software  version control system Git,Github</li>
                         <li>Familiarity with the whole web stack</li>
                         <li>knowladge of NodeJS and express</li>
                         <li>experiance with SASS</li>
                         <li>Knowledge of bundling tools webpack,roll up and parcel</li>
+                        <li>UI/UX basics</li>
                     </ul>
                     <h2 className="skills-secondaryTitle">Other Programming skills</h2>
                     <ul className="skills-list">
