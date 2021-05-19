56e75958bcea3bdcad492f6ae1fdef3d1a044d4d
sharlq <shehabakhader@gmail.com>
2021-05-19 17:54:53 +0300

    chainging the lay out

--- src/components/Myprojects/Myprojects.jsx
+++ src/components/Myprojects/Myprojects.jsx
@@ -16,7 +16,7 @@ const Myprojects = () => {
                 Here is a group of my projects to show my abilites and what iam capable of doing 
               </p>
             </div>
-            <Card cardStyle={{height:'23.3vh',width:'23.3vw'}}>
+            <Card yourClass="card0" cardStyle={{height:'23.3vh',width:'23.3vw'}}>
               <CardContent
                cardTitle={card[0].title}
                github={card[0].github}
@@ -24,7 +24,7 @@ const Myprojects = () => {
                cardImage={card[0].image}
                cardImageRatio={card[0].imageRatio} />
             </Card>
-            <Card cardStyle={{height:'22vh',width:'22vw'}} >
+            <Card yourClass="card1" cardStyle={{height:'22vh',width:'22vw'}} >
             <CardContent
                cardTitle={card[1].title}
                cardDescription={card[1].description}
@@ -33,7 +33,7 @@ const Myprojects = () => {
                cardImage={card[1].image}
                cardImageRatio={card[1].imageRatio} />
             </Card>
-            <Card cardStyle={{height:'25vh',width:'25vw'}}>
+            <Card yourClass="card2" cardStyle={{height:'25vh',width:'25vw'}}>
             <CardContent
                cardTitle={card[2].title}
                cardDescription={card[2].description}
@@ -42,7 +42,7 @@ const Myprojects = () => {
                cardImage={card[2].image}
                cardImageRatio={card[2].imageRatio} />
             </Card>
-            <Card cardStyle={{height:'22vh',width:'22vw'}}>
+            <Card yourClass="card1" cardStyle={{height:'22vh',width:'22vw'}}>
             <CardContent
                cardTitle={card[4].title}
                cardDescription={card[4].description}
@@ -54,7 +54,7 @@ const Myprojects = () => {
             <p className="myProjects-projectsGrid_description">
                 this group of  projects to give an idea about my level as a web developer 
               </p>
-            <Card cardStyle={{height:'23.5vh',width:'23.5vw',marginRight:"1.5vh"}}>
+            <Card yourClass="card0" cardStyle={{height:'23.5vh',width:'23.5vw',marginRight:"1.5vh"}}>
             <CardContent
                cardTitle={card[5].title}
                cardDescription={card[5].description}
@@ -63,7 +63,7 @@ const Myprojects = () => {
                cardImage={card[5].image}
                cardImageRatio={card[5].imageRatio} />
             </Card>
-            <Card cardStyle={{height:'25vh',width:'25vw',marginLeft:"1.5vh"}}>
+            <Card yourClass="card2" cardStyle={{height:'25vh',width:'25vw',marginLeft:"1.5vh"}}>
             <CardContent
                cardTitle={card[3].title}
                cardDescription={card[3].description}
