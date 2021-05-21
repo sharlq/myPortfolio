65a8e9adef1b7692fcb2fcfeb600a7baa427a93f
sharlq <shehabakhader@gmail.com>
2021-05-21 17:01:57 +0300

    finalizing the project

--- src/components/Myprojects/Card.jsx
+++ src/components/Myprojects/Card.jsx
@@ -7,22 +7,14 @@ import { useSpring, animated } from 'react-spring';
 function Card({ children,cardStyle={},yourClass=""}) {
 
     const classes = `card ${yourClass}`
-    // We add this ref to card element and use in onMouseMove event ...
-    // ... to get element's offset and dimensions.
+ 
     const ref = useRef();
-  
-    // Keep track of whether card is hovered so we can increment ...
-    // ... zIndex to ensure it shows up above other cards when animation causes overlap.
+
     const [isHovered, setHovered] = useState(false);
   
     const [animatedProps, setAnimatedProps] = useSpring(() => {
       return {
-        // Array containing [rotateX, rotateY, and scale] values.
-        // We store under a single key (xys) instead of separate keys ...
-        // ... so that we can use animatedProps.xys.interpolate() to ...
-        // ... easily generate the css transform value below.
         xys: [0, 0, 1],
-        // Setup physics
         config: { mass: 10, tension: 400, friction: 40, precision: 0.00001 }
       };
     });
@@ -34,38 +26,33 @@ function Card({ children,cardStyle={},yourClass=""}) {
         
         onMouseEnter={() => setHovered(true)}
         onMouseMove={({ clientX, clientY }) => {
-          // Get mouse x position within card
+
           const x =
             clientX -
             (ref.current.offsetLeft -
               (window.scrollX || window.pageXOffset || document.body.scrollLeft));
   
-          // Get mouse y position within card
+
           const y =
             clientY -
             (ref.current.offsetTop -
               (window.scrollY || window.pageYOffset || document.body.scrollTop));
   
-          // Set animated values based on mouse position and card dimensions
-          const dampen = 50; // Lower the number the less rotation
+         
+          const dampen = 50; 
           const xys = [
-            -(y - ref.current.clientHeight / 2) / dampen, // rotateX
-            (x - ref.current.clientWidth / 2) / dampen, // rotateY
-            1.07 // Scale
+            -(y - ref.current.clientHeight / 2) / dampen, 
+            (x - ref.current.clientWidth / 2) / dampen,
+            1.07 
           ];
-  
-          // Update values to animate to
           setAnimatedProps.start({ xys: xys });
         }}
         onMouseLeave={() => {
           setHovered(false);
-          // Set xys back to original
           setAnimatedProps.start({ xys: [0, 0, 1] });
         }}
         style={{...{
-          // If hovered we want it to overlap other cards when it scales up
           zIndex: isHovered ? 2 : 1,
-          // Interpolate function to handle css changes
           transform: animatedProps.xys.to(
             (x, y, s) =>
               `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
